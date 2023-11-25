71cee188642b66e97298c7afa77912af1c258e5f
Tyler Meuse <tmeuze@a64.one>
2023-11-25 16:12:56 -0500

    update-install.bat

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@
 
 powershell -Command "& {
     Get-ChildItem -Path 'C:\BITSTREAM\WAU\INSTALL' -Recurse | Unblock-File;
-    $arguments = '-Silent', '-UseWhiteList', '-NotificationLevel', 'None', '-UpdatesInterval', 'Daily', '-ListPath', 'https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/CONFIG/APPS/', '-ModsPath', 'https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/CONFIG/MODS/';
-    Start-Process -FilePath 'powershell.exe' -Verb RunAs -ArgumentList '-noprofile', '-executionpolicy', 'bypass', '-file', 'C:\BITSTREAM\WAU\INSTALL\Winget-AutoUpdate-Install.ps1', $arguments
-}"
+    $arguments = '-Silent -UseWhiteList -NotificationLevel None -UpdatesInterval Daily -ListPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/CONFIG/APPS/ -ModsPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/CONFIG/MODS/';
+    Start-Process -FilePath 'powershell.exe' -Verb RunAs -ArgumentList ('-noprofile', '-executionpolicy', 'bypass', '-file', 'C:\BITSTREAM\WAU\INSTALL\Winget-AutoUpdate-Install.ps1 ' + $arguments)}
+"
