57ee112cb4c66b8e27393829363966e0b0b1c061
Tyler Meuse <tmeuze@a64.one>
2023-11-25 16:01:25 -0500

    fix-install.bat

--- install.bat
+++ install.bat
@@ -4,13 +4,13 @@
 :: Put WAU Arguments here ::
 ::::::::::::::::::::::::::::
 
-SET arguments=-Silent -UseWhiteList -NotificationLevel None -UpdatesInterval Daily -ListPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/CONFIG/APPS/ -ModsPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/COFIG/MODS/
+SET arguments=-Silent -UseWhiteList -NotificationLevel None -UpdatesInterval Daily -ListPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/CONFIG/APPS/ -ModsPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/CONFIG/MODS/
 
 
 ::::::::::::::::::::::::::::
 :: Run Powershell Script  ::
 ::::::::::::::::::::::::::::
 
-@REM SET PowershellCmd=Start-Process powershell.exe -Verb RunAs -Argument -noprofile -executionpolicy bypass -file C:\BITSTREAM\WAU\INSTALL\Winget-AutoUpdate-Install.ps1 %arguments%
+SET PowershellCmd=Start-Process powershell.exe -Verb RunAs -ArgumentList "-noprofile -executionpolicy bypass -file C:\BITSTREAM\WAU\INSTALL\Winget-AutoUpdate-Install.ps1 %arguments%"
 
-powershell -Command "& {Get-ChildItem -Path 'C:\BITSTREAM\WAU\INSTALL' -Recurse | Unblock-File; Start-Process powershell.exe -Verb RunAs -Argument -noprofile -executionpolicy bypass -file C:\BITSTREAM\WAU\INSTALL\Winget-AutoUpdate-Install.ps1 %arguments%}"
+powershell -Command "& {Get-ChildItem -Path 'C:\BITSTREAM\WAU\INSTALL' -Recurse | Unblock-File; %PowershellCmd%}"
