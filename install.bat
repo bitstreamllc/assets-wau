177bda3a8e5c3701e51bc018f7dbaf6324240fd8
Tyler Meuse <tmeuze@a64.one>
2023-11-25 19:25:31 -0500

    debug-scripts

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
-SET arguments=-Silent -NotificationLevel None -UpdatesInterval Daily -ListPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/CONFIG/APPS/ -ModsPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/COFIG/MODS/
+SET Arguments=-Silent -NotificationLevel None -UpdatesInterval Daily -ListPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/CONFIG/APPS/ -ModsPath https://raw.githubusercontent.com/bitstreamllc/assets-wau/master/COFIG/MODS/
 SET ScriptPath=C:\BITSTREAM\WAU\INSTALL\Winget-AutoUpdate-Install.ps1
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Verb RunAs -WindowStyle hidden -File %ScriptPath% %arguments%
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File %ScriptPath% %Arguments%
 pause
